120cf0bf607d15dbeed3abf46fb7621c133f0ab0
Benjamin Asbach <1482768+asbachb@users.noreply.github.com>
2026-03-25 23:59:03 +0100

    WW

--- hosts/cobalt.impl.it/users/poddy/home.nix
+++ hosts/cobalt.impl.it/users/poddy/home.nix
@@ -1,18 +1,19 @@
 { pkgs, inputs, ...}:
 let
   echo-server = import ./containers/echo-server/container.nix;
+  echo-server2 = import ./containers/echo-server2/container.nix;
 in
 {
     home-manager.users.poddy = {
     home = {
       packages = [
-        pkgs.htop
+        #pkgs.htop
       ];
       stateVersion = "25.11";
     };
     
     imports = [ inputs.quadlet-nix.homeManagerModules.quadlet ];
 
-    virtualisation.quadlet.containers = echo-server;
+    virtualisation.quadlet.containers = echo-server // echo-server2;
   };
 }
